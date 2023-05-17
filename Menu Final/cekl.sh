974fbe00c707aca8854a77344c697a1cc07e2528
kanghory <132531297+kanghory@users.noreply.github.com>
2023-05-17 14:05:17 +0700

    Update cekl.sh

--- Menu Final/cekl.sh
+++ Menu Final/cekl.sh
@@ -64,7 +64,7 @@ done
 rm -rf /tmp/other.txt
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                 •  Kang Hory  •                  $COLOR1│$NC"
+echo -e "$COLOR1│${NC}                 •  Kang Hory VPN Premium •                  $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
